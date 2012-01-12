d027a8313ca4c23becf1a35c346677d2578b8a41
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2012-01-12 13:50:17 +0000

    * spřístupnění SQL pro čtení a testy
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@2392 c08143d5-b1a1-4836-b697-8134d2fdba83

--- ys.php
+++ ys.php
@@ -9,31 +9,53 @@
 
   // Ans(w)er rozeznává tyto doplňkové parametry v URL
   //   dbs=db_name:other_db_name,...    -- záměna MySQL tabulek za jiné (je zpracováváno ve fis.ini)
+
   require_once("$app.inc");
-  require_once("ezer2/server/ae_slib.php");
+  require_once("{$EZER->version}/server/ae_slib.php");
   $app_name.= $EZER->options->mysql ? " - {$EZER->options->mysql}" : '';
 
-  $js= array(
-    'ezer2/client/licensed/ckeditor/ckeditor.js',
-    'ezer2/client/licensed/clientcide.js',
-    'ezer2/client/licensed/mootools/asset.js',
-    'ezer2/client/licensed/mootools/slider.js',
-    'ezer2/client/licensed/glfx.js',
-    'ezer2/client/lib.js',
-    'ezer2/client/ezer_fdom1.js',
-    'ezer2/client/ezer.js',
-    'ezer2/client/ezer_report.js',
-    'ezer2/client/ezer_fdom2.js',
-    'ezer2/client/app.js',
-    'db/db_fce.js',
-    'ds/fce.js',
-    'http://maps.googleapis.com/maps/api/js?sensor=false'
+  $client= "{$EZER->version}/client";
+  $licensed= "$client/licensed";
+  $js= array_merge(
+    // ckeditor a mootools
+    array("$licensed/ckeditor/ckeditor.js","$licensed/clientcide.js"),
+    // pro verzi 2.1
+    $EZER->version=='ezer2'
+    ? array("$licensed/mootools/asset.js","$licensed/mootools/slider.js"):array(),
+    // pro verzi 2.2
+    $EZER->version=='ezer2.2'
+    ? array("$licensed/datepicker.js"):array(),
+    // jádro Ezer
+    array("$client/lib.js","$client/ezer_fdom1.js","$client/ezer.js","$client/ezer_report.js",
+      "$client/ezer_fdom2.js","$client/app.js","$licensed/zeroclipboard/ZeroClipboard.js"),
+    // další knihovny
+    array("ezer2/client/licensed/glfx.js","http://maps.googleapis.com/maps/api/js?sensor=false"),
+    // uživatelské skripty
+    array("db/db_fce.js","ds/fce.js")
   );
+
+//   $js= array(
+//     'ezer2/client/licensed/ckeditor/ckeditor.js',
+//     'ezer2/client/licensed/clientcide.js',
+//     'ezer2/client/licensed/mootools/asset.js',
+//     'ezer2/client/licensed/mootools/slider.js',
+//     'ezer2/client/licensed/glfx.js',
+//     'ezer2/client/lib.js',
+//     'ezer2/client/ezer_fdom1.js',
+//     'ezer2/client/ezer.js',
+//     'ezer2/client/ezer_report.js',
+//     'ezer2/client/ezer_fdom2.js',
+//     'ezer2/client/app.js',
+//     'db/db_fce.js',
+//     'ds/fce.js',
+//     'http://maps.googleapis.com/maps/api/js?sensor=false'
+//   );
+
   $css= array(
-    './ezer2/client/ezer.css.php',
-    './ezer2/client/licensed/fancyupload.css',
-    './ys/ys.css.php',
-    './db/db.css.php'
+    "./$client/ezer.css.php",
+    "./$client/licensed/fancyupload.css",
+    "./ys/ys.css.php",
+    "./db/db.css.php"
   );
   $options= (object)array(
     'skill'      => "'y'",
