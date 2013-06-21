3c0259bdef4f11d65045555aeea7c21ef01278e7
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2013-06-21 12:01:11 +0000

    * úpravy výpisů I
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@3094 c08143d5-b1a1-4836-b697-8134d2fdba83

--- db/db_fce.js
+++ db/db_fce.js
@@ -157,7 +157,7 @@ Ezer.fce.rc2roky= function (rc) {
       var now= new Date();
       var nar= new Date(y,m,d);
 //       roku= now.getFullYear() - y;
-      roku= Math.floor((now-nar)/(1000*60*60*24*365.26));
+      roku= Math.floor((now-nar)/(1000*60*60*24*365.2425));
     }
   }
   return roku;
@@ -172,7 +172,7 @@ Ezer.fce.roku= function (dat,rok) {
     var m= dat.split('.');
     if ( m.length==3 ) {
       var nar= new Date(m[2],m[1]-1,m[0]);
-      roku= (now-nar)/(1000*60*60*24*365.26);
+      roku= (now-nar)/(1000*60*60*24*365.2425);
       roku= roku<10 ? Math.round(10*roku)/10 : Math.round(roku);
       if ( !roku ) roku= 1;       // první rok počítej i načatý
     }
@@ -183,7 +183,7 @@ Ezer.fce.roku= function (dat,rok) {
       m= dat.split('-');        // sql-formát
       if ( m.length==3 ) {
         var nar= new Date(m[0],m[1]-1,m[2]);
-        roku= (now-nar)/(1000*60*60*24*365.26);
+        roku= (now-nar)/(1000*60*60*24*365.2425);
         roku= roku<10 ? Math.round(10*roku)/10 : Math.round(roku);
         if ( !roku ) roku= 1;       // první rok počítej i načatý
       }
