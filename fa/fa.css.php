d9e5273c351efdf86812f8feed16d1c49305d91e
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2014-12-01 07:49:28 +0000

    * RC1
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@3659 c08143d5-b1a1-4836-b697-8134d2fdba83

--- fa/fa.css.php
+++ fa/fa.css.php
@@ -15,7 +15,7 @@ require_once("../skins/colors.php");
 echo <<<__EOD
 /* úpravy standardu */
 .BrowseSmart td.BrowseQry input {
-  background-color:#effdf1; }
+  background-color:$b8_brow; }
 /*.SelectDrop li {
   white-space:normal !important; }*/
 .Label, .Check {
@@ -36,7 +36,7 @@ echo <<<__EOD
 .ae_butt_on {
   cursor:default; background-color:$b_work; z-index:0; border-radius:5px; text-align:center;  }
 .ae_butt_off {
-  cursor:default; background-color:#effdf1; z-index:0; border-radius:5px; text-align:center;  }
+  cursor:default; background-color:$b8_brow; z-index:0; border-radius:5px; text-align:center;  }
 .ae_butt_on:hover {
   background:url("../../skins/ch/label_switch_on_hover.png") repeat-x scroll 0 -1px transparent !important; }
 .ae_butt_off:hover {
