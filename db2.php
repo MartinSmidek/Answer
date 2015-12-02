d3ed5c421ccc5300132288051352bfaa8acec4e8
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2015-12-02 21:18:30 +0000

    * db2 verze s upozorněním
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@4099 c08143d5-b1a1-4836-b697-8134d2fdba83

--- db2.php
+++ db2.php
@@ -1,4 +1,60 @@
-<?php # Systém An(w)er/Ezer/YMCA Familia, (c) 2008-2014 Martin Šmídek <martin@smidek.eu>
+<?php # Systém An(w)er/YMCA Setkání/YMCA Familia, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
+
+if ( !isset($_COOKIE['ezer_db2_go']) ) {
+
+echo(<<<__EOD
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr" slick-uniqueid="1" spellcheck="false">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=9">
+  <link rel="shortcut icon" href="./db2/img/db2.png">
+  <title>Answer instalace</title>
+  <script>
+    var createCookie = function(name, value, days) {
+      var expires;
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+      else {
+        expires = "";
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+    }
+  </script>
+ </head>
+ <body style="padding:100px">
+  <div style="background-color:yellow; border:1px solid red; width:700px">
+   <div style="float:right;padding:5px;">2.12.2015</div>
+   <div style="padding:50px;">
+    Vážení klienti, milí kamarádi,
+    <br><br>na serveru je nainstalována nová verze systému Answer.
+    Obsahuje podle dohody výborů obou organizací spojenou databázi YMCA Setkání a YMCA Familia
+    (zvolené řešení je korektní vzhledem k ochraně osobních dat účastníků našich akcí).
+    <br>
+    <br>Vyzkoušejte si prosím novou podobu Answeru nejprve nanečisto v testovacím prostředí (1),
+    teprve až si vše vyzkoušíte, začněte pracovat v ostré verzi (2).
+    Změny do ostré verze ale prosím dělejte až po Mikuláši.
+    Pokud byste potřebovali nějakou informaci nebo vysvětlení, zavolejte mi prosím na 603 150 565,
+    nebo pište na <a href="mailto:martin@smidek.eu">můj mail</a>.
+    <br><br>
+    Přeji vám příjemnou práci a dobrý adventní čas
+    <br>Martin
+    <br><br>
+    <button onclick="window.location.href='http://answer.setkani.org/db2_test.php';">(1) Vyzkouším si nové prostředí v testovací databázi, s daty mohu dělat cokoliv mě napadne</button>
+    <br><br>
+    <button onclick="createCookie('ezer_db2_go',1,1);window.location.href='http://answer.setkani.org/db2.php';">(2) Chci vyzkoušet ostrou verzi ale nebudu do ní před Mikulášem nic zapisovat ani mazat</button>
+   </div>
+  </div>
+ </body>
+</html>
+__EOD
+);
+
+}
+else {
+  setcookie ("ezer_db2_go", "", time() - 3600);
 
   # inicializace systémů Ans(w)er
   #   $app        = kořenová podsložka aplikace ... db2
@@ -40,4 +96,5 @@
     "db2/db2.css"
   );
   answer_php($app,"Answer ...",'ezer_db2','db',$js,$css,$options);
+}
 ?>
