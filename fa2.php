3d3baed1d6966d190e3643d61837433a028ea192
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2015-12-02 21:18:30 +0000

    * db2 verze s upozorněním
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@4099 c08143d5-b1a1-4836-b697-8134d2fdba83

--- fa2.php
+++ fa2.php
@@ -1,4 +1,5 @@
 <?php # Systém An(w)er/Ezer/YMCA Familia, (c) 2008-2014 Martin Šmídek <martin@smidek.eu>
+
 die(<<<__EOD
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr" slick-uniqueid="1" spellcheck="false">
  <head>
@@ -6,39 +7,66 @@ die(<<<__EOD
   <meta http-equiv="X-UA-Compatible" content="IE=9">
   <link rel="shortcut icon" href="./db2/img/db2.png">
   <title>Answer instalace</title>
+  <script>
+    var createCookie = function(name, value, days) {
+      var expires;
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+      else {
+        expires = "";
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+    }
+  </script>
  </head>
  <body style="padding:100px">
   <div style="background-color:yellow; border:1px solid red; width:700px">
-   <div style="float:right;padding:5px;">2.12.2015 - 12:00</div>
+   <div style="float:right;padding:5px;">2.12.2015</div>
    <div style="padding:50px;">
     Vážení klienti, milí kamarádi,
-    <br>s mírným skluzem dnes na serveru probíhá instalace nové verze systému Answer.
-    <br>Omlouvám se, ale dnes již nebude možné se systémem pracovat.
-    <br>Pokud byste přesto potřebovali nějakou informaci, zavolejte mi prosím na 603 150 565.
-    <br>Zítra ráno bude Answer opět v provozu.
+    <br><br>na serveru je nainstalována nová verze systému Answer.
+    Obsahuje podle dohody výborů obou organizací spojenou databázi YMCA Setkání a YMCA Familia
+    (zvolené řešení je korektní vzhledem k ochraně osobních dat účastníků našich akcí).
+    <br>
+    <br>Vyzkoušejte si prosím novou podobu Answeru nejprve nanečisto v testovacím prostředí (1),
+    teprve až si vše vyzkoušíte, začněte pracovat v ostré verzi (2).
+    Změny do ostré verze ale prosím dělejte až po Mikuláši.
+    Pokud byste potřebovali nějakou informaci nebo vysvětlení, zavolejte mi prosím na 603 150 565,
+    nebo pište na <a href="mailto:martin@smidek.eu">můj mail</a>.
+    <br><br>
+    Přeji vám příjemnou práci a dobrý adventní čas
     <br>Martin
+    <br><br>
+    <button onclick="window.location.href='http://answer.setkani.org/db2_test.php';">(1) Vyzkouším si nové prostředí v testovací databázi, s daty mohu dělat cokoliv mě napadne</button>
+    <br><br>
+    <button onclick="createCookie('ezer_db2_go',1,1);window.location.href='http://answer.setkani.org/db2.php';">(2) Chci vyzkoušet ostrou verzi ale nebudu do ní před Mikulášem nic zapisovat ani mazat</button>
    </div>
   </div>
  </body>
 </html>
 __EOD
 );
-  # inicializace systémů Ans(w)er
-  #   $app        = kořenová podsložka aplikace ... ys/ys2/fa/fa2/cr
-  #   $app_name   = jméno aplikace
-  #   $db_name    = hlavní databáze ... bylo-li v URL &test=1 přidá se do options.tabu_db
-  #   $skin       = ck|ch
-  #   $js_lib     = pole s *.js
-  #   $css_lib    = pole s *.css
-  #   $options    = doplnění Ezer.options
 
-  require_once("answer.php");
-  $options= (object)array(
-    'web'        => "'http://www.familia.cz'", // web organizace - pro odmítnutí odhlášení
-    'skill'      => "'f'",
-    'autoskill'  => "'!f'"
-  );
-//   $css= array("./ezer2.2/client/ezer.css.php","./db/db.css.php","./fa/fa.css.php");
-  $css= array("skins/ch/ch.ezer.css");
-  answer_php('fa2','Ans(w)er - Familia','ezer_fa','ch',array(),$css,$options);
+//   # inicializace systémů Ans(w)er
+//   #   $app        = kořenová podsložka aplikace ... ys/ys2/fa/fa2/cr
+//   #   $app_name   = jméno aplikace
+//   #   $db_name    = hlavní databáze ... bylo-li v URL &test=1 přidá se do options.tabu_db
+//   #   $skin       = ck|ch
+//   #   $js_lib     = pole s *.js
+//   #   $css_lib    = pole s *.css
+//   #   $options    = doplnění Ezer.options
+//
+//   require_once("answer.php");
+//   $options= (object)array(
+//     'web'        => "'http://www.familia.cz'", // web organizace - pro odmítnutí odhlášení
+//     'skill'      => "'f'",
+//     'autoskill'  => "'!f'"
+//   );
+// //   $css= array("./ezer2.2/client/ezer.css.php","./db/db.css.php","./fa/fa.css.php");
+//   $css= array("skins/ch/ch.ezer.css");
+//   answer_php('fa2','Ans(w)er - Familia','ezer_fa','ch',array(),$css,$options);
+
 ?>
