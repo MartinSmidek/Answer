5debb40b4df1213ecc0130e4a397b141413edf91
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2016-07-28 15:05:05 +0000

    * oprava exportu do xlsx - a upgrade xls2 na verzi 1.8.0
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@4370 c08143d5-b1a1-4836-b697-8134d2fdba83

--- db2/db2_fce.php
+++ db2/db2_fce.php
@@ -5907,7 +5907,7 @@ __XLS;
     fce_error($inf);
   }
   else {
-    $html= " Výpis byl vygenerován ve formátu <a href='docs/$name.xls' target='xls'>Excel</a>.";
+    $html= " Výpis byl vygenerován ve formátu <a href='docs/$name.xlsx' target='xlsx'>Excel</a>.";
   }
   $result->html= $html;
   return $result;
