ce5237bd5b1ea4367698c1cfc8ca59123b4f7f3a
gandi <gandi@c08143d5-b1a1-4836-b697-8134d2fdba83>
2017-01-16 10:58:32 +0000

    * starty CR2 ...
    git-svn-id: svn://192.168.1.145/ezer/trunk/ys2@4498 c08143d5-b1a1-4836-b697-8134d2fdba83

---
+++ cr2.php
@@ -0,0 +1,42 @@
+<?php # Systém An(w)er/YMCA Setkání/YMCA Familia, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
+
+  # inicializace systémů Ans(w)er
+  #   $app        = kořenová podsložka aplikace ... db2
+  #   $app_name   = jméno aplikace
+  #   $db_name    = hlavní databáze ... bylo-li v URL &test=1 přidá se do options.tabu_db
+  #   $skin       = ck|ch
+  #   $js_lib     = pole s *.js
+  #   $css_lib    = pole s *.css
+  #   $options    = doplnění Ezer.options
+
+  $app=  'cr2';
+  $app_name= "<span style:red>testování Answer CPR</span>";
+
+  require_once("answer.php");
+  $options= (object)array(
+//     'watch_access' => 0,
+    'watch_access' => 4,
+    'watch_access_opt' => // ... barvení v Uživatelé + select v ezer2.syst.ezer
+       "{name:{4:'CPR Ostrava'},
+         abbr:{4:'C'},
+         css:{4:'ezer_ys'}}",
+    'web'          => "''", // web organizace - pro odmítnutí odhlášení
+    'skill'        => "'d'",
+    'autoskill'    => "'!d'"
+  );
+
+  $cookie= 4;
+  $app_last_access= "{$app}_last_access";
+  if ( isset($_COOKIE[$app_last_access])
+    && $_COOKIE[$app_last_access]>0 ) { //&&  $_COOKIE[$app_last_access]<4 ) {
+    $cookie= $_COOKIE[$app_last_access];
+  }
+  $choice_css= "skins/ck/ck.ezer.css=skin";
+
+  $js= array(
+  );
+  $css= array($choice_css,
+    "db2/db2.css"
+  );
+  answer_php($app,$app_name,'ezer_cr2','ck',$js,$css,$options);
+?>
