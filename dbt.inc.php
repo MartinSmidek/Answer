82d5ef7de1c0a3af021645d11b5be30318cf384c
MartinSmidek <martin@smidek.eu>
2021-07-22 12:40:56 +0200

    * dbt jinak

--- dbt.inc.php
+++ dbt.inc.php
@@ -1,7 +1,7 @@
 <?php # Systém An(w)er/Ezer/YMCA Familia a YMCA Setkání, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
 
   # nastavení systému Ans(w)er před voláním AJAX
-  #   $app        = kořenová podsložka aplikace ... db2
+  #   $app        = kořenová podsložka aplikace ... dbt
   #   $answer_db  = logický název hlavní databáze (s případným '_test')
   #   $dbs_plus   = pole s dalšími databázemi ve formátu $dbs
   #   $php_lib    = pole s *.php - pro 'ini'
@@ -18,7 +18,7 @@
   $rel_root=     $_SESSION[$ezer_root]['rel_root'];
   chdir($abs_root);
 
-//  echo("db2.inc.php start, server=$ezer_server");
+//  echo("dbt.inc.php start, server=$ezer_server");
 
   // inicializace objektu Ezer
   $EZER= (object)array(
@@ -33,11 +33,23 @@
   // databáze
   $deep_root= "../files/answer";
   require_once("$deep_root/dbt.dbs.php");
+  
+  $path_backup= "$deep_root/sql";
+  
+  // cesta k utilitám MySQL/MariaDB
+  $ezer_mysql_path= array(
+      "C:/Apache/bin/mysql/mysql5.7.21/bin",  // *.bean
+      "/volume1/@appstore/MariaDB/usr/bin",   // Synology DOMA
+      "/volume1/@appstore/MariaDB/usr/bin"   // Synology YMCA
+    )[$ezer_server];
 
   // definice zápisů do _track
   $tracking= '_track';
   $tracked= ',akce,pobyt,spolu,osoba,tvori,rodina,_user,';
   
+  // moduly interpreta zahrnuté do aplikace - budou zpracovány i reference.i_doc pro tabulky kompilátoru
+  $ezer_comp_root= "db2/ds_fce3";
+
   // definice modulů specifických pro Answer
   $k= substr($kernel,4,1)=='3' ? '3' : '';
   $app_php= array(
@@ -56,43 +68,9 @@
   chdir($abs_root);
   require_once("{$EZER->version}/ezer_ajax.php");
 
-//  echo("db2.inc.php end<br>");
+//  echo("dbt.inc.php end<br>");
   // SPECIFICKÉ PARAMETRY
   global $USER;
   $VPS= isset($USER) ? ($USER->org==1 ? 'VPS' : 'PPS') : 'VPS';
   $EZER->options->org= 'YMCA Familia, YMCA Setkání';
-
-
-/*
-  # nastavení systému Ans(w)er před voláním AJAX
-  #   $app        = kořenová podsložka aplikace ... db2
-  #   $answer_db  = logický název hlavní databáze (s případným '_test')
-  #   $dbs_plus   = pole s dalšími databázemi ve formátu $dbs
-  #   $php_lib    = pole s *.php - pro 'ini'
-
-  require_once("answer.inc.php");
-  $dbs_plus= array(
-    array( // ostré
-      'setkani' =>      array(0,'192.168.1.146','gandi','','cp1250','setkani',1)), // 1=vynechat zálohování
-    array( // lokální
-      'setkani' =>      array(0,'localhost','gandi','radost','cp1250','setkani',1))        // 1=vynechat zálohování
-  );
-  
-  global $EZER;
-  answer_ezer('dbt');
-  $db2_ys2_fcex= $EZER->version=='ezer3.1' ? 'db2_ys2_fce3' : 'db2_ys2_fce';
-  
-  $php= array(
-    "db2/$db2_ys2_fcex.php"
-  );
-  
-  $ezer= array(
-  );
-  answer_ini('dbt','ezer_db2',$dbs_plus,$php,$ezer);
-
-  // SPECIFICKÉ PARAMETRY
-  global $USER;
-  $VPS= isset($USER) ? ($USER->org==1 ? 'VPS' : 'PPS') : 'VPS';
-  $EZER->options->org= 'YMCA Familia, YMCA Setkání';
-*/
 ?>
--- dbt.php
+++ dbt.php
@@ -1,7 +1,7 @@
 <?php # Systém An(w)er/YMCA Setkání/YMCA Familia, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
 
   # inicializace systémů Ans(w)er
-  #   $app        = kořenová podsložka aplikace ... db2
+  #   $app        = kořenová podsložka aplikace ... dbt
   #   $app_name   = jméno aplikace
   #   $db_name    = hlavní databáze ... bylo-li v URL &test=1 použije se databáze s postfixem _test
   #   $skin       = ck|ch
@@ -11,23 +11,28 @@
 
   $kernel= "ezer3.1"; 
   $ezer_server= 
-    $_SERVER["SERVER_NAME"]=='answer.bean'    ? 0 : (        // 0:lokální 
-    $_SERVER["SERVER_NAME"]=='xxx.setkani.org' ? 1 : (       // 1:Lukáš - dead
-    $_SERVER["SERVER_NAME"]=='answer.setkani.org' ? 2 : -1));// 2:Synology
+    $_SERVER["SERVER_NAME"]=='answer.bean'        ? 0 : (      // 0:lokální 
+    $_SERVER["SERVER_NAME"]=='answer.doma'        ? 1 : (      // 1:Synology DOMA
+    $_SERVER["SERVER_NAME"]=='answer.setkani.org' ? 2 : -1));  // 2:Synology YMCA
 
-  // parametry aplikace Answer/db2
+  // parametry aplikace Answer/dbt
   $app_name=  "Answer-test";
   $app= $app_root=  'dbt';
 
-  $title_style= $ezer_server ? '' : "style='color:#ef7f13'";
-  $title_flag=  $ezer_server ? '' : 'lokální ';
+  $title_style= $ezer_server==1 ? "style='color:#0094FF'" : (
+                $ezer_server==0 ? "style='color:#ef7f13'" : '');
+  $title_flag=  $ezer_server==2 ? '' : 'lokální ';
   $CKEditor= isset($_GET['editor']) ? $_GET['editor'] : '4.6';
   // pro ezer2.2 nutno upravit ezer_main, ezer_ajax, ae_slib
   $kernel=   "ezer".(isset($_GET['ezer'])?$_GET['ezer']:'3.1'); 
-
+  
   // nastav jako default PDO=2
   if ( !isset($_GET['pdo']))
     $_GET['pdo']= 2;
+  
+  // nastav &touch=0 pro Windows
+  if (strtoupper(substr(PHP_OS,0,3))==='WIN') 
+    $_GET['touch']= 0;
 
   // ochránění přímého přístupu do složek s .htaccess/RewriteCond "%{HTTP_COOKIE}" "!EZER"
   setcookie("EZER",$app,0,"/");
@@ -40,28 +45,33 @@
   // cesty
   $abs_roots= array(
       "C:/Ezer/beans/answer",
-      "/home/www/ezer/www-ys/2",
-      "/volume1/web/www/answer");
-//      "/var/services/web/www/answer");
+      "/volume1/web/www/answer",
+      "/volume1/web/www/answer"
+    );
   $rel_roots= array(
       "http://answer.bean:8080",
-      "https://answer.setkani.org",
-      "https://answer.setkani.org");
+      "http://answer.doma",
+      "https://answer.setkani.org"
+    );
   $rel_root= $rel_roots[$ezer_server];
+  $path_foto= "{$abs_roots[$ezer_server]}/fotky";
   $path_akce= array(
       "D:/MS/",
-      "",
+      "/volume1/YS/",
       "/volume1/YS/"
     )[$ezer_server];
   
   // upozornění na testovací verzi
   $demo= '';
   if ( $ezer_server==2 ) {
-    $click= "jQuery('#DEMO').fadeOut(500).delay(20000).fadeIn(2000);";
+    // zmizí a zase se objeví
+    $click= "jQuery('#DEMO').fadeOut(500).delay(300000).fadeIn(2000);";
+    // jen zmizí
+//    $click= "jQuery('#DEMO').fadeOut(500);";
     $dstyle= "left:0; top:0; position:fixed; transform:rotate(320deg) translate(-128px,-20px); "
         . "width:500px;height:100px;background:orange; color:white; font-weight: bolder; "
         . "text-align: center; font-size: 40px; line-height: 96px; z-index: 16; opacity: .5;";
-    $demo= "<div id='DEMO' onmouseover=\"$click\" style='$dstyle'>nový server</div>";
+    $demo= "<div id='DEMO' onmouseover=\"$click\" style='$dstyle'>test MariaDB.10</div>";
   }
 
   // skin a css
@@ -79,7 +89,7 @@
     <span $title_style>"
     . $title_flag
     ."<span id='access' onclick=\"$choice_js\" oncontextmenu=\"$choice_js\">
-        $app_name$v $access_app
+        Answer$v $access_app
       </span>
       <div id='access_menu'>
         <span onclick='personify(1);'>Ans(w)er Setkání</span>
@@ -101,12 +111,11 @@
     $cookie==2 ? "ch" : "db" );
 
   $k= substr($kernel,4,1)=='3' ? '3' : '';
-  $app_js= array("/db2/ds_fce$k.js","/db2/db2_fce$k.js");
+  $app_js= array("db2/ds_fce$k.js","db2/db2_fce$k.js");
   
   $app_css= [ // $choice_css,
-//      "/db2/db2.css",
-      $kernel=='ezer3.1' ? "$rel_root/db2/db2.css.php=skin" : "$rel_root/db2/db2.css",
-      "/$kernel/client/wiki.css"
+      $kernel=='ezer3.1' ? "db2/db2.css.php=skin" : "db2/db2.css",
+      "$kernel/client/wiki.css"
    ];
   //  require_once("answer.php");
   $add_options= (object) [
@@ -114,6 +123,7 @@
     'curr_version' => 1,                // při přihlášení je nahrazeno nejvyšší ezer_kernel.version
     'watch_access' => 3,
     'path_akce'    => "'$path_akce'", // absolutní cesta do složky Akce
+    'path_foto'    => "'$path_foto'", // absolutní cesta do složky fotky
     'group_db'     => "'ezer_answer'",
     'watch_access_opt' => // ... barvení v Uživatelé + select v ezer2.syst.ezer
        "{name:{1:'Setkání',2:'Familia',3:'Setkání+Familia'},
@@ -152,56 +162,8 @@
       }
     }"
   );
-//  echo("db2.php end<br>");
+//  echo("dbt.php end<br>");
   // je to aplikace se startem v kořenu
   require_once("$kernel/ezer_main.php");
 
-/*
-  # inicializace systémů Ans(w)er
-  #   $app        = kořenová podsložka aplikace ... db2
-  #   $app_name   = jméno aplikace
-  #   $db_name    = hlavní databáze ... bylo-li v URL &test=1 použije se databáze s postfixem _test
-  #   $skin       = ck|ch
-  #   $js_lib     = pole s *.js
-  #   $css_lib    = pole s *.css
-  #   $options    = doplnění Ezer.options
-
-  $app=  'dbt';
-
-  require_once("answer.php");
-  $options= (object) ['watch_access' => 3,
-    'watch_access_opt' => // ... barvení v Uživatelé + select v ezer2.syst.ezer
-       "{name:{1:'Setkání',2:'Familia',3:'Setkání+Familia'},
-         abbr:{1:'S',2:'F',3:'S+F'},
-         css:{1:'ezer_ys',2:'ezer_fa',3:'ezer_db'}}",
-    'web'          => "''", // web organizace - pro odmítnutí odhlášení
-    'skill'        => "'d'",
-    'autoskill'    => "'!d'",
-    'db_test'      => 1
-   ];
-
-  $cookie= 3;
-  $app_last_access= "{$app}_last_access";
-  
-  $coo= filter_input(INPUT_COOKIE,$app_last_access,FILTER_VALIDATE_INT);
-  if ( $coo && $coo>0 && $coo<4 ) {
-    $cookie= $coo;
-  }
-  $get_ev= filter_input(INPUT_GET,'ezer',FILTER_SANITIZE_SPECIAL_CHARS);
-  $ev= $get_ev=='3.1' ? '3' : '';
-  $choice_css=
-    $cookie==1 ? "skins/ck/ck.ezer$ev.css=skin" : (
-    $cookie==2 ? "skins/ch/ch.ezer$ev.css=skin" : "skins/db/db.ezer$ev.css=skin" );
-  $skin=
-    $cookie==1 ? "ck" : (
-    $cookie==2 ? "ch" : "db" );
-
-  $js= array(
-    $get_ev==3 ? "db2/ds_fce3.js" : "db2/ds_fce.js"
-  );
-  $css= [$choice_css,
-    "db2/db2.css"
-   ];
-  answer_php($app,"Answer ...",'ezer_db2',$skin,$js,$css,$options);
-*/
-?>  
+?>
