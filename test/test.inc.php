059185e04b4870208708ff29a97c45b31057c7f8
MartinSmidek <martin@smidek.eu>
2022-01-07 10:34:34 +0100

    * skrytí přístupových údajů do files

--- test/test.inc.php
+++ test/test.inc.php
@@ -1,9 +1,15 @@
 <?php # (c) 2010-2019 Martin Smidek <martin@smidek.eu>
 
+  // klíče
+  $deep_root= "../files/tut";
+  require_once("$deep_root/tut.dbs.php");
+  
   global // import 
     $ezer_root; 
   global // export
     $EZER, $ezer_server;
+  global // klíče
+    $api_gmail_user, $api_gmail_pass;
   
   // vyzvednutí ostatních hodnot ze SESSION
   $ezer_server=  $_SESSION[$ezer_root]['ezer_server'];
@@ -19,11 +25,17 @@
       'activity'=>(object)array(),
       'CMS'=>(object)array(
         'GMAIL'=>(object)array(
-          'mail'=>'answer@setkani.org', // adresa odesílatele mailů
+          'mail'=>$api_gmail_user, // adresa odesílatele mailů
           'name'=>'YMCA Setkání', 
-          'pswd'=>'answer2017'
+          'pswd'=>$api_gmail_pass
       ))
   );
+
+  // informace pro debugger o poloze ezer modulů
+  $dbg_info= (object)array(
+    'src_path'  => array('test','dbt','ezer3.1') // poloha a preference zdrojových modulů
+  );
+
   $db= array('ezer_test','ezer_test','ezer_test','ezer_test');
   $dbs= array(
     array(
@@ -46,12 +58,11 @@
   
   // specifické PHP moduly
   $app_php=   array(
-//      'tut/tut.cg.php',
+      'test/test_fce.php'
 //      'tut/tut.the.php','tut/tut.bib.php','tut/tut.sand.php',
 //      'ezer3.1/server/sys_doc.php','ezer3.1/server/ezer_ruian.php'
       );
   
   // aplikace se startem v podsložce
   require_once("{$EZER->version}/ezer_ajax.php");
-
 
