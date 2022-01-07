825868a0faf46eae607235b42a069db5637406a7
MartinSmidek <martin@smidek.eu>
2022-01-07 11:07:38 +0100

    * skrytí přístupových údajů do files

--- test/test.inc.php
+++ test/test.inc.php
@@ -1,8 +1,8 @@
 <?php # (c) 2010-2019 Martin Smidek <martin@smidek.eu>
 
   // klíče
-  $deep_root= "../files/tut";
-  require_once("$deep_root/tut.dbs.php");
+  $deep_root= "../files/test";
+  require_once("$deep_root/test.dbs.php");
   
   global // import 
     $ezer_root; 
@@ -36,26 +36,6 @@
     'src_path'  => array('test','dbt','ezer3.1') // poloha a preference zdrojových modulů
   );
 
-  $db= array('ezer_test','ezer_test','ezer_test','ezer_test');
-  $dbs= array(
-    array(
-      'ezer_test'     => array(0,'localhost','gandi','radost','utf8','ezer_tutorial'),
-      'ezer_kernel'   => array(0,'localhost','gandi','radost','utf8','ezer_tutorial'),
-      'ezer_system'   => array(0,'localhost','gandi','radost','utf8','ezer_tutorial')),
-    array(
-      'ezer_test'     => array(0,'localhost','gandi','r8d0st','utf8','ezer_tutorial'),
-      'ezer_kernel'   => array(0,'localhost','gandi','r8d0st','utf8','ezer_tutorial'),
-      'ezer_system'   => array(0,'localhost','gandi','r8d0st','utf8','ezer_tutorial')),
-    array( // Synology YMCA
-      'ezer_test'     => array(0,'ymca','ymca','JW4YNPTDf4Axkj9','utf8','ezer_tutorial'),
-      'ezer_kernel'   => array(0,'ymca','ymca','JW4YNPTDf4Axkj9','utf8'),
-      'ezer_system'   => array(0,'ymca','ymca','JW4YNPTDf4Axkj9','utf8','ezer_tutorial')),
-    array( // Synology DOMA
-      'ezer_test'     => array(0,'localhost','gandi','radost','utf8','ezer_tutorial'),
-      'ezer_kernel'   => array(0,'localhost','gandi','radost','utf8','ezer_tutorial'),
-      'ezer_system'   => array(0,'localhost','gandi','radost','utf8','ezer_tutorial'))
-  );
-  
   // specifické PHP moduly
   $app_php=   array(
       'test/test_fce.php'
