7aa9dfb72214863a8e02b25952e6fe8d1b8615ea
Martin Šmídek <martin@smidek.eu>
2024-02-23 15:24:11 +0100

    * online přihláška - typ MS,LK - RC0

--- prihlaska.php
+++ prihlaska.php
@@ -37,7 +37,7 @@ if (!ip_ok()) {
 //$testovaci_mail= 'anabasis@seznam.cz';        $TEST= 3; // známá rodina ale bez ženy
 //$testovaci_mail= 'frantisekbezdek@atlas.cz';  $TEST= 3; // známá osoba ale bez rodiny
 //$testovaci_mail= 'kancelar@setkani.org';      $TEST= 3; 
-$testovaci_mail= 'nemo2@smidek.eu';         $TEST= 3; // neznámý mail
+//$testovaci_mail= 'nemo2@smidek.eu';         $TEST= 3; // neznámý mail
 //$TEST= 2;
 if (!isset($testovaci_mail)) {
   $TEST= $_GET['test'] ?? ($_SESSION[$AKCE]->test ?? $TEST);
