ec989cde50914c356c624f4b76b0e310936f7db7
Martin Šmídek <martin@smidek.eu>
2025-03-11 17:00:09 +0100

    * přihlášky - pretty log

--- db2/db2_ys2_fce3.php
+++ db2/db2_ys2_fce3.php
@@ -3397,6 +3397,7 @@ end:
 # --------------------------------------------------------------------------------------- prihl open
 # vrátí seznam otevřených přihlášek dané akce
 function prihl_open($ida,$hotove=1) { trace();
+  $n= $nm= 0;  // n, n-mobil
   $HAVING= $hotove
       ? "HAVING _naakci!=0"
       : "HAVING _stavy NOT REGEXP '^ok|,ok|-ok' AND _naakci=0";
@@ -3405,6 +3406,7 @@ function prihl_open($ida,$hotove=1) { trace();
         ,IFNULL(MAX(id_rodina),0) AS _rodina,IFNULL(GROUP_CONCAT(DISTINCT nazev),'?') AS _nazev
         ,IFNULL(MAX(o.id_osoba),0) AS _osoba,IFNULL(CONCAT(o.prijmeni,' ',o.jmeno),'?')
         ,DATE_FORMAT(MIN(open),'<b>%d.%m</b> %H:%i') AS _open
+        -- ,stav
         ,GROUP_CONCAT(DISTINCT stav ORDER BY p.id_prihlaska) AS _stavy
         ,TRIM(GROUP_CONCAT(DISTINCT LEFT(browser,4) SEPARATOR ' '))
         ,MAX(p.id_prihlaska) AS _id_prihlaska
@@ -3412,7 +3414,8 @@ function prihl_open($ida,$hotove=1) { trace();
         ,COUNT(*) AS x, MIN(open) AS _open_
       FROM prihlaska AS p
       LEFT JOIN rodina USING (id_rodina)
-      LEFT JOIN osoba AS o ON o.email LIKE CONCAT('%',p.email,'%')
+      -- LEFT JOIN osoba AS o ON o.email LIKE CONCAT('%',p.email,'%')
+      LEFT JOIN osoba AS o ON o.email REGEXP CONCAT('(^|[;,\\\\s]+)',p.email,'($|[;,\\\\s]+)')
       LEFT JOIN pobyt AS pa ON pa.id_akce=$ida 
       LEFT JOIN spolu AS s ON s.id_osoba=o.id_osoba AND pa.id_pobyt=s.id_pobyt
       WHERE p.id_akce=$ida AND p.email!='' -- AND p.email NOT REGEXP '(smidek)'
@@ -3421,19 +3424,26 @@ function prihl_open($ida,$hotove=1) { trace();
       $HAVING
       ORDER BY _open_ DESC");
   while ($rp && (list($email,$naakci,$idr,$rodina,$ido,$osoba,$kdy,$stavy,$jak,$idw,$idp)= pdo_fetch_array($rp))) {
+    $real_idp= select('IFNULL(id_pobyt,0)','pobyt',"id_pobyt=$idp");
     $_ido= $ido ? tisk2_ukaz_osobu($ido) : '';
     $_idr= $idr ? tisk2_ukaz_rodinu($idr) : '';
-    $_idw= $idw ? tisk2_ukaz_prihlasku($idw,$ida,$idp,'','',$idw) : $idw;
-    $pokusy= substr($stavy,0,50).(substr($stavy,50) ? ' ...' : '');
-    $row= "<tr><td title='$stavy' align='right'>$_idw => </td><td>$kdy</td><td title='$jak'>$email</td>"
+    $_idw= $idw ? tisk2_ukaz_prihlasku($idw,$ida,$real_idp,'','',$idw) : $idw;
+//    $pokusy= substr($stavy,0,50).(substr($stavy,50) ? ' ...' : '');
+    $skrt= $real_idp || !$hotove ? '' : ' style=text-decoration:line-through';
+    $row= "<tr$skrt><td title='$stavy' align='right'>$_idw => </td><td>$kdy</td><td title='$jak'>$email</td>"
         . "<td>$osoba $_ido</td><td>$rodina $_idr</td>"
-        . ( $ido ? '' : "<td title='$stavy'>$pokusy</td>")
+//        . ( $ido ? '' : "<td title='$stavy'>$pokusy</td>")
         . "<td>$jak</td></tr>";
-    if (!$ido || preg_match("/novi|novacci/",$stavy)) $novi.= "\n$row"; else $znami.= "\n$row";
+    if (preg_match("/REG/",$stavy)) $novi.= "\n$row"; else $znami.= "\n$row";
+//    if (!$ido || preg_match("/novi|novacci/",$stavy)) $novi.= "\n$row"; else $znami.= "\n$row";
 //    if ($ido) $znami.= "\n$row"; else $novi.= "\n$row";
+    $n++;
+    $nm+= preg_match('/^[AI]/',$jak) ? 1 : 0;
   }
   $Jake= $hotove ? "Dokončené" : "Nedokončené";
-  $html.= "<h3>$Jake přihlášky nováčků</h3><table>$novi</table>";
+  $mobilem= round(100*$nm/$n);
+  $html.= "<h3>Celkem $mobilem% mobilem</h3>";
+  $html.= "<h3>$Jake přihlášky nově registrovaných</h3><table>$novi</table>";
   $html.= "<h3>$Jake přihlášky známých</h3><table>$znami</table>";
   return $html;
 }
