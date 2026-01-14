bb4f6c4469ebf9c7170e2cdb4b3b97a89272366d
MartinSmidek <martin@smidek.eu>
2026-01-14 10:39:51 +0100

    * úprava startovacích skriptů pro ms2

--- ms2.inc.php
+++ ms2.inc.php
@@ -1,6 +1,114 @@
 <?php # Systém An(w)er/Ezer/YMCA Familia a YMCA Setkání, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
 
   # nastavení systému Ans(w)er před voláním AJAX
+  #   $app        = kořenová podsložka aplikace ... db2
+  #   $answer_db  = logický název hlavní databáze (s případným '_test')
+  #   $dbs_plus   = pole s dalšími databázemi ve formátu $dbs
+  #   $php_lib    = pole s *.php - pro 'ini'
+
+  $test= '';
+
+  global // import 
+    $ezer_root; 
+  global // export
+    $EZER, $ezer_server, $ezer_version;
+  global // klíče
+    $api_gmail_user, $api_gmail_pass;
+  
+  // vyzvednutí ostatních hodnot ze SESSION
+  $ezer_server=  $_SESSION[$ezer_root]['ezer_server'];
+  $ezer_version= $_SESSION[$ezer_root]['ezer'];
+  $abs_root=     $_SESSION[$ezer_root]['abs_root'];
+  $rel_root=     $_SESSION[$ezer_root]['rel_root'];
+  chdir($abs_root);
+
+  // živá větev v github
+  $git_app_branch= $test?'test':'master';   // místo defaultu 'master'
+
+  // informace pro debugger o poloze ezer modulů
+  $dbg_info= (object)array(
+    'src_path'  => array('ms2','db2',"ezer$ezer_version") // poloha a preference zdrojových modulů
+  );
+
+  $ezer_root_js= 'db2'; // informace pro sys_doc.php
+
+  // databáze
+  $deep_root= "../files/answer";
+  require_once($test?"$deep_root/ms2t.dbs.php":"$deep_root/ms2.dbs.php");
+  
+  $path_backup= "$deep_root/sql";
+  
+  // inicializace objektu Ezer
+  $EZER= (object)array(
+      'version'=>"ezer{$_SESSION[$ezer_root]['ezer']}",
+      'options'=>(object)array(
+          'mail' => "martin@smidek.eu",
+          'phone' => "603&nbsp;150&nbsp;565",
+          'author' => "Martin"
+      ),
+      'activity'=>(object)array(),
+    );
+
+  // informace pro debugger o poloze ezer modulů
+  $dbg_info= (object)array(
+    'src_path'  => array('ms2','db2',"ezer$ezer_version") // poloha a preference zdrojových modulů
+  );
+
+  // definice zápisů do _track
+  $tracking= '_track';
+  $tracked= ',akce,pobyt,spolu,osoba,tvori,rodina,_user,'
+      . 'join_platba,platba,cenik,pdenik,';
+  $tracked_id= ['akce'=>'id_duakce'];
+  $tracked_view= [];
+  
+  // moduly interpreta zahrnuté do aplikace - budou zpracovány i reference.i_doc pro tabulky kompilátoru
+  $ezer_comp_root= "db2/ds_fce3";
+
+  // definice modulů specifických pro Answer
+  $app_php= array(
+    "db2/db-akce.php",
+    "db2/db-bank.php",
+    "db2/db-cenik.php",
+    "db2/db-data.php",
+    "db2/db-dotaznik.php",
+    "db2/db-dum.php",
+    "db2/db-elim.php",
+    "db2/db-evid.php",
+    "db2/db-foto.php",
+    "db2/db-lib.php",
+    "db2/db-mail.php",
+    "db2/db-mapa.php",
+    "db2/db-pece.php",
+    "db2/db-pokl.php",
+    "db2/db-prihl.php",
+    "db2/db-stat.php",
+    "db2/db-tisk.php",
+    "db2/db-ucast.php",
+    "db2/db-obsolete.php",
+//    "db2/db-delete.php",
+    "db2/db2_tcpdf.php",
+//    "ezer$ezer_version/server/ezer_ruian.php",
+//    "ezer$ezer_version/server/ezer_cms3.php"
+  );
+
+  $ezer= array(
+  );
+  
+  // PDF knihovny
+  require_once('tcpdf/tcpdf.php');
+ 
+  // je to aplikace se startem v rootu
+  chdir($abs_root);
+  require_once("{$EZER->version}/ezer_ajax.php");
+
+  // SPECIFICKÉ PARAMETRY
+  global $USER;
+  $VPS= 'PPS';
+  $EZER->options->org= 'MS severní Čechy';
+
+/* OLD ms2.inc.php
+
+  # nastavení systému Ans(w)er před voláním AJAX
   #   $answer_db  = logický název hlavní databáze 
 
   global // import 
@@ -88,3 +196,5 @@
   global $USER;
   $VPS= 'PPS';
   $EZER->options->org= 'MS severní Čechy';
+
+*/
--- ms2.php
+++ ms2.php
@@ -1,5 +1,116 @@
 <?php # Systém An(w)er/YMCA Setkání/YMCA Familia, (c) 2008-2015 Martin Šmídek <martin@smidek.eu>
 
+  // časová značka při spuštění
+  date_default_timezone_set("Europe/Prague");
+  file_put_contents("last_access.txt",date('Y-m-d H:i:s'));
+
+  # inicializace systémů Ans(w)er
+  #   $app        = kořenová podsložka aplikace ... ms2
+  #   $app_name   = jméno aplikace
+  #   $db_name    = hlavní databáze 
+  #   $skin       = tt|default|default
+  #   $js_lib     = pole s *.js
+  #   $css_lib    = pole s *.css
+  #   $options    = doplnění Ezer.options
+
+  // verze použitého jádra Ezeru
+  $ezer_version= isset($_GET['ezer']) ? $_GET['ezer'] : '3.3'; 
+  $test= '';
+
+  // server, databáze, cesty, klíče
+  $deep_root= "../files/answer";
+  require_once($test?"$deep_root/ms2t.dbs.php":"$deep_root/ms2.dbs.php");
+
+  // parametry aplikace Answer/ms2
+  $app_name=  "Answer$test";
+  $ezer_root= $app= $app_root=  'ms2'.($test?'t':'');
+  
+  // nastav jako default PDO=2
+  $_GET['pdo']= 2;
+  
+  $title_style= $ezer_server==1 ? "style='color:#0094FF'" : (
+                $ezer_server==0 ? "style='color:#ef7f13'" : '');
+  $title_flag=  in_array($ezer_server,[0,4]) ? 'lokální ' : '';
+
+  $CKEditor= isset($_GET['editor']) ? $_GET['editor'] : '4.6';
+  
+  // nastav &touch=0 pro Windows
+  if (strtoupper(substr(PHP_OS,0,3))==='WIN') 
+    $_GET['touch']= 0;
+  $touch= isset($_GET['touch']) ? $_GET['touch'] : 0;
+
+  // ochránění přímého přístupu do složek s .htaccess/RewriteCond "%{HTTP_COOKIE}" "!EZER"
+  setcookie("EZER",$app,0,"/");
+
+  // upozornění na testovací verzi
+  $click= "jQuery('#DEMO').fadeOut(500);";
+  $dstyle= "left:-50px; bottom:0; position:fixed; transform:rotate(40deg) translate(-107px,-14px); "
+      . "width:500px;height:80px;background:orange; color:white; font-weight: bolder; "
+      . "text-align: center; font-size: 40px; line-height: 75px; z-index: 16; opacity: .5;";
+  $demo= "<div id='DEMO' onmouseover=\"$click\" style='$dstyle'>testovací data</div>";
+
+  // skin a css
+  $new= '<sub><small> '.$ezer_version.($touch?' touch':'').'</small></sub>';
+  $title= "$demo
+    <span $title_style>"
+    . $title_flag
+    . "$app_name$new  MS"
+    ."</span>";
+  $skin= "tt";
+
+  $app_js= array("db2/ds_fce3.js","db2/db2_fce3.js");
+
+  $app_css= [ 
+      "db2/db2.css.php=skin",
+      "ezer$ezer_version/client/wiki.css"
+   ];
+
+  $add_options= (object) [
+    'watch_git'    => 1,                // sleduj git-verzi aplikace a jádra, při změně navrhni restart
+    'curr_version' => 1,                // při přihlášení je nahrazeno nejvyšší ezer_kernel.version
+    'watch_access' => 8,                // 8
+    'path_akce'    => "'$path_akce'", // absolutní cesta do složky Akce
+    'path_foto'    => "'$path_foto'", // absolutní cesta do složky fotky
+    'group_db'     => "'ezer_answer'",
+    'watch_access_opt' => // ... barvení v Uživatelé + select v ezer2.syst.ezer
+       "{name:{8:'Šance pro manželství z.s.'},
+         abbr:{8:'M'},
+         css:{8:'ezer_ms'}}",
+    'web'          => "'manzelskasetkani.cz'", // web organizace - pro odmítnutí odhlášení
+    'skill'        => "'d'",
+    'autoskill'    => "'!d'",
+    'db_test'      => 0,
+  ];
+
+  // (re)definice Ezer.options
+  $add_pars= array(
+    'favicon' => $favicon,
+    'watch_pin' => 1,   // true = povolit dvoufázové přihlašování pomocí _user.usermail a PINu
+    'watch_key' => 1,   // true = nebo povolit přístup jen po vložení klíče
+    'watch_ip' => 1,    // true = jinak povolit přístup jen ze známých IP adres
+    'title_right' => $title,
+//    'contact' => $kontakt,
+    'CKEditor' => "{
+      version:'$CKEditor',
+      Minimal:{toolbar:[['Bold','Italic','Source']]},
+      IntranetSlim:{
+        toolbar:[['Bold','Italic','-','Link','Unlink','-','Source']],
+        removePlugins:'wsc,elementspath,scayt'
+      },
+      'EzerMail':{toolbar:[['PasteFromWord',
+        '-','Format','Bold','Italic','TextColor','BGColor',
+        '-','JustifyLeft','JustifyCenter','JustifyRight',
+        '-','Link','Unlink','HorizontalRule','Image',
+        '-','NumberedList','BulletedList',
+        '-','Outdent','Indent',
+        '-','Source','ShowBlocks','RemoveFormat']]
+      }
+    }"
+  );
+  // je to standardní aplikace se startem v kořenu
+  require_once("ezer$ezer_version/ezer_main.php");
+
+/* OLD ms2.php
   # inicializace systémů Ans(w)er
   #   $app        = kořenová podsložka aplikace ... db2
   #   $app_name   = jméno aplikace
@@ -111,4 +222,4 @@
 
   // je to aplikace se startem v kořenu
   require_once("ezer$ezer_version/ezer_main.php");
-
+*/
