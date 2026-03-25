b96c5761d731b37d4c5394bd2f7d1a661ed913b8
MartinSmidek <martin@smidek.eu>
2026-03-25 08:31:53 +0100

    * bude použita přihláška 5.1

--- prihlaska_2025.php
+++ prihlaska_2025.php
@@ -3,18 +3,7 @@
   (c) 2025 Martin Smidek <martin@smidek.eu>
 
   přepínač na asktuální verzi online přihlašování pro YMCA Setkání (jen obnovy a LK MS YS)
-  debuger je lokálne nastaven pro verze PHP: 7.2.33 - musí být ručně spuštěn Chrome
 
  */
 
-if ($_SERVER['REMOTE_ADDR']??0 == '127.0.0.1') {
-//  $_GET['test']= 2; // NEZAPISOVAT! a trasovat
-  $_GET['test']= 1; // zapisovat a trasovat
-  $_GET['mail']= 0; // NEZASÍLAT!
-}
-else {
-//  $_GET['test']= 0; $_GET['mail']= 1; // ZASÍLAT + ZAPISOVAT
-}
-
-  if (!isset($_GET['org'])) $_GET['org']= 1;
-  require_once("prihlaska_2025.4.php"); // odkazem z www.setkani.org
+  require_once("prihlaska_2026.php"); // aktuální verze
--- prihlaska_2026.php
+++ prihlaska_2026.php
@@ -3,7 +3,6 @@
   (c) 2025-2026 Martin Smidek <martin@smidek.eu>
 
   přepínač na aktuální verzi online přihlašování pro YMCA Setkání a YMCA Familia 
-  debuger je lokálne nastaven pro verze PHP: 7.2.33 - musí být ručně spuštěn Chrome
 
  */
 
@@ -11,4 +10,4 @@
 //  $_GET['test']= 2; $_GET['mail']= 0; // NEZASÍLAT + NEZAPISOVAT
 
 if (!isset($_GET['org'])) $_GET['org']= 1; 
-require_once("prihlaska_2025.4.php"); // odkazem z www.setkani.org
+require_once("prihlaska_2025.5.php"); // odkazem z www.setkani.org
